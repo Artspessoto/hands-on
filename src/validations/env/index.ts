528613b1f68994e7a43032d8031c3781288e2244
Arthur Spessoto <artspessoto@gmail.com>
2025-06-10 19:50:13 -0300

    feat: update envSchema with encryption key (32 bytes) and iv key (16 bytes) and add new environments to .env.example

--- src/validations/env/index.ts
+++ src/validations/env/index.ts
@@ -4,6 +4,12 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
   PORT: z.coerce.number().default(3333),
+  ENCRYPTION_KEY: z
+    .string()
+    .length(32, "A ENCRYPTION_KEY deve ter exatamente 32 caracteres (bytes)"),
+  IV_KEY: z
+    .string()
+    .length(16, "A IV_KEY deve ter exatamente 16 caracteres (bytes)"),
 });
 
 const _env = envSchema.safeParse(process.env);
