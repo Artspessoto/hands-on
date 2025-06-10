0fe99c82ef66864e14f8413f869bd18fe8ed74e5
Arthur Spessoto <artspessoto@gmail.com>
2025-06-10 16:17:42 -0300

    feat: add PrismaClient instance with conditional logging

---
+++ src/database/prismaClient.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { env } from "../env";
+
+const isDev = env.NODE_ENV == "dev";
+
+const prisma = new PrismaClient({
+  log: isDev
+    ? [
+        { emit: "event", level: "query" },
+        { emit: "event", level: "error" },
+        { emit: "event", level: "info" },
+      ]
+    : [{ emit: "event", level: "error" }],
+});
+
+if (isDev) {
+  prisma.$on("query", (e) => {
+    console.log("🟢 Query:" + e.query);
+    console.log("🟡 Params:" + e.params);
+    console.log("⏱ Duration:" + e.duration + "ms");
+  });
+}
+
+prisma.$on("error", (e) => {
+  console.error("🔴 Prisma Error:", e.message);
+});
+
+export default prisma;
