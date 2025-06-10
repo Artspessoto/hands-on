85fa38cd423cc6b3bd8d8a810ccfd03a6c4dbd6c
Arthur Spessoto <artspessoto@gmail.com>
2025-06-10 16:53:55 -0300

    feat: add validation of user CPF

---
+++ src/utils/isValidCPF.ts
@@ -0,0 +1,26 @@
+export default function isValidCPF(cpf: string): boolean {
+  const cleanCPF = cpf.replace("/D/g", "");
+
+  if (cleanCPF.length !== 11 || /^(\d)\1{10}$/.test(cleanCPF)) {
+    return false;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < 9; i++) {
+    sum += parseInt(cleanCPF.charAt(i)) * (10 - 1);
+  }
+
+  let rest = (sum * 10) % 11;
+  if (rest == 10 || rest == 11) rest = 0;
+  if (rest !== parseInt(cleanCPF.charAt(9))) return false;
+
+  sum = 0;
+  for (let i = 0; i < 10; i++) {
+    sum += parseInt(cleanCPF.charAt(i)) * (11 - i);
+  }
+
+  rest = (sum * 10) % 11;
+  if (rest === 10 || rest === 11) rest = 0;
+
+  return rest === parseInt(cleanCPF.charAt(10));
+}
