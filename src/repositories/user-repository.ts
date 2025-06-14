d928c6464d6f2874f8a461798175718e1afdb32f
Arthur Spessoto <artspessoto@gmail.com>
2025-06-14 01:04:24 -0300

    feat: add resources to find by user id and update user data by id

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -3,11 +3,27 @@ import { User } from "../validations/schemas/user-schema";
 
 class UserRepository {
   async findByEmail(email: string) {
-    return prisma.user.findUnique({ where: { email } });
+    return await prisma.user.findUnique({ where: { email } });
+  }
+  async findByUser(userId: string) {
+    return await prisma.user.findUnique({
+      where: { id: userId },
+    });
   }
   async create(data: User) {
     const user = await prisma.user.create({ data });
     return user;
+  }
+  async update(data: User, userId: string) {
+    const user = await prisma.user.update({
+      where: { id: userId },
+      data,
+    });
+    return user;
+  }
+  async list() {
+    const users = await prisma.user.findMany();
+    return users;
   }
 }
 
