3a1c53d3acd0d1bd4a59bc178718a5aba29cc7b6
Arthur Spessoto <artspessoto@gmail.com>
2025-06-14 01:48:45 -0300

    feat: use cookie and jwt plugins into app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import routes from "./routes";
 import { errorHandler } from "./utils/errorHandler";
 import cors from "@fastify/cors";
+import { cookiePlugin } from "./plugins/cookiePlugin";
+import { jwtPlugin } from "./plugins/jwtPlugin";
 
 export const app = fastify();
 
@@ -11,6 +13,9 @@ app.register(cors, {
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
 });
+
+cookiePlugin(app);
+jwtPlugin(app);
 
 app.register(routes);
 app.setErrorHandler(errorHandler);
