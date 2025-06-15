99ce550dadb24f0b168850c6a29bd89a31675a60
Arthur Spessoto <artspessoto@gmail.com>
2025-06-15 02:35:18 -0300

    feat: add schema to Users (create) for swagger api

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,26 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import UserController from "../controllers/user-controller";
+import fromZodSchema from "zod-to-json-schema";
+import { publicUserSchema } from "../validations/schemas/user-schema";
 
 const userController = new UserController();
+const userBodySchema = fromZodSchema(publicUserSchema);
 
 async function userRoutes(fastify: FastifyInstance) {
-  fastify.post("/", async (req: FastifyRequest, reply: FastifyReply) => {
-    return userController.create(req, reply);
-  });
+  fastify.post(
+    "/",
+    {
+      schema: {
+        body: userBodySchema,
+        response: { 201: userBodySchema },
+        description: "Cria um usuário (voluntário ou doador)",
+        tags: ["Users"],
+      },
+    },
+    async (req: FastifyRequest, reply: FastifyReply) => {
+      return userController.create(req, reply);
+    }
+  );
 }
 
 export default userRoutes;
