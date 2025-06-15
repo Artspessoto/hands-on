352c12b45648e8a6c89589d650ea54eb1c7365be
Arthur Spessoto <artspessoto@gmail.com>
2025-06-15 12:51:20 -0300

    feat: add schema to Sessions for swagger api

--- src/routes/session.routes.ts
+++ src/routes/session.routes.ts
@@ -1,12 +1,33 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import SessionController from "../controllers/session-controller";
+import fromZodSchema from "zod-to-json-schema";
+import { sessionSchema } from "../validations/schemas/session-schema";
 
 const sessionController = new SessionController();
+const sessionBodySchema = fromZodSchema(sessionSchema);
 
 async function sessionRoutes(fastify: FastifyInstance) {
-  fastify.post("/", async (req: FastifyRequest, reply: FastifyReply) => {
-    return sessionController.create(req, reply);
-  });
+  fastify.post(
+    "/",
+    {
+      schema: {
+        body: sessionBodySchema,
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              token: { type: "string" },
+            },
+          },
+        },
+        description: "Autentica um usuário no app",
+        tags: ["Sessions"],
+      },
+    },
+    async (req: FastifyRequest, reply: FastifyReply) => {
+      return sessionController.create(req, reply);
+    }
+  );
 }
 
 export default sessionRoutes;
